15758f9cefb86238b1416ad647e98edc1f94738f
cagri <cagri.uysal07@gmail.com>
2019-12-06 13:42:09 +0300

    api model defined

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+  price = models.CharField(max_length=50)
+  year = models.CharField(max_length=50)
+  model = models.CharField(max_length=50)
+  ext_color = models.CharField(max_length=50)
+  int_color = models.CharField(max_length=50)
+  transmission = models.CharField(max_length=50)
+  contact = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.model
