be9f7de596fbe52795b71a4695104c6160b95df0
cagri <cagri.uysal07@gmail.com>
2019-12-06 21:08:46 +0300

    filtering for api added

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 
 from .models import Car
@@ -6,3 +7,5 @@ from .serializers import CarSerializer
 class CarAPIView(generics.ListAPIView):
   queryset = Car.objects.all()
   serializer_class = CarSerializer
+  filter_backends = [DjangoFilterBackend]
+  filterset_fields = ['brand', 'price', 'year', 'model', 'ext_color', 'int_color', 'transmission', 'contact'] 
